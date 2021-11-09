672fc041ab1bee9a1635b92e2f7d4b3d35f0f9fa
mjkabir40 <mjkabir40@gmail.com>
2021-11-08 19:14:09 -0500

    fix lint issue for now

--- src/config/config.js
+++ src/config/config.js
@@ -1,26 +1,28 @@
-const dotenv = require('dotenv');
-const Joi = require('joi');
-const path = require('path');
+const dotenv = require("dotenv");
+const Joi = require("joi");
+const path = require("path");
 
-dotenv.config({ path: path.join(__dirname, '../../.env') });
+dotenv.config({ path: path.join(__dirname, "../../.env") });
 
 const envVarsSchema = Joi.object()
   .keys({
     PORT: Joi.number().default(3000),
-    NODE_ENV: Joi.string().valid('prod', 'test', 'dev').required(),
-    ACCESS_TOKEN_SECRET: Joi.string().required().description('Access token'),
+    NODE_ENV: Joi.string().valid("prod", "test", "dev").required(),
+    ACCESS_TOKEN_SECRET: Joi.string().required().description("Access token"),
     PERSONALIZER_SERVICE_KEY: Joi.string()
       .required()
-      .description('personalizer key'),
+      .description("personalizer key"),
     PERSONALIZER_BASE_URL: Joi.string()
       .required()
-      .description('personalizer endpoint'),
+      .description("personalizer endpoint"),
   })
   .unknown();
 
 const { value: envVars, error } = envVarsSchema
-  .prefs({ errors: { label: 'key' } })
+  .prefs({ errors: { label: "key" } })
   .validate(process.env);
+
+console.log("Config validation error: " + error.message);
 
 module.exports = {
   port: envVars.PORT,
